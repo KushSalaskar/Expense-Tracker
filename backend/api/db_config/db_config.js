a08757d48b9f93353adad36834667db05d3ae990
KushSalaskar <kushsalaskar@gmail.com>
2024-11-10 21:37:25 -0800

    moved passwords to dotenv

--- backend/api/db_config/db_config.js
+++ backend/api/db_config/db_config.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
-const dbName = process.env.DB_NAME || 'expense_tracker';
-const dbUser = process.env.DB_USER || 'kushsalaskar';
-const dbPassword = process.env.DB_PASSWORD || 'password';
-const dbHost = process.env.DB_HOST || 'localhost';
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
 
 const db = new Sequelize(dbName, dbUser, dbPassword, {
     host: dbHost,
